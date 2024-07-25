934f1d1b355edd494219218c6a532b5db34ddc70
Vihang Patel <vihangp@zignuts.com>
2024-07-25 15:17:27 +0530

    design the cart component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,11 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { toggleCart, removeItem, incrementItem, decrementItem } from '../store/slices/cartSlice';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  toggleCart,
+  removeItem,
+  incrementItem,
+  decrementItem,
+} from "../store/slices/cartSlice";
 
 const Cart = () => {
   const { isCartOpen, cartItems } = useSelector((state) => state.cart);
@@ -26,11 +31,15 @@ const Cart = () => {
   // Disable body scroll when the cart is open
   useEffect(() => {
     const docBody = document.body;
-    isCartOpen ? docBody.classList.add('overflow_hide') : docBody.classList.remove('overflow_hide');
+    isCartOpen
+      ? docBody.classList.add("overflow_hide")
+      : docBody.classList.remove("overflow_hide");
   }, [isCartOpen]);
 
   const cartQuantity = cartItems.length;
-  const cartTotal = cartItems.map(item => item.price * item.quantity).reduce((prevValue, currValue) => prevValue + currValue, 0);
+  const cartTotal = cartItems
+    .map((item) => item.price * item.quantity)
+    .reduce((prevValue, currValue) => prevValue + currValue, 0);
 
   return (
     <>
@@ -38,8 +47,14 @@ const Cart = () => {
         <div id="cart" className="cart">
           <div className="cart_content">
             <div className="cart_head">
-              <h2>Cart <small>({cartQuantity})</small></h2>
-              <div title="Close" className="close_btn" onClick={() => handleCloseCart(false)}>
+              <h2>
+                Cart <small>({cartQuantity})</small>
+              </h2>
+              <div
+                title="Close"
+                className="close_btn"
+                onClick={() => handleCloseCart(false)}
+              >
                 <span>&times;</span>
               </div>
             </div>
@@ -48,7 +63,7 @@ const Cart = () => {
               {cartQuantity === 0 ? (
                 <h2>Cart is empty</h2>
               ) : (
-                cartItems.map(item => {
+                cartItems.map((item) => {
                   const { id, img, title, price, quantity } = item;
                   const itemTotal = price * quantity;
 
@@ -60,16 +75,34 @@ const Cart = () => {
 
                       <div className="cart_items_info">
                         <h4>{title}</h4>
-                        <h3 className="price">₹ {itemTotal.toLocaleString()}</h3>
+                        <h3 className="price">
+                          ₹ {itemTotal.toLocaleString()}
+                        </h3>
                       </div>
 
                       <div className="cart_items_quantity">
-                        <button type="button" className="btn btn-decrement" onClick={() => handleDecrement(id)}>-</button>
+                        <button
+                          type="button"
+                          className="btn btn-decrement"
+                          onClick={() => handleDecrement(id)}
+                        >
+                          -
+                        </button>
                         <span className="quantity">{quantity}</span>
-                        <button type="button" className="btn btn-increment" onClick={() => handleIncrement(id)}>+</button>
+                        <button
+                          type="button"
+                          className="btn btn-increment"
+                          onClick={() => handleIncrement(id)}
+                        >
+                          +
+                        </button>
                       </div>
 
-                      <div title="Remove Item" className="cart_items_delete" onClick={() => handleRemove(id)}>
+                      <div
+                        title="Remove Item"
+                        className="cart_items_delete"
+                        onClick={() => handleRemove(id)}
+                      >
                         <span>&times;</span>
                       </div>
                     </div>
@@ -84,7 +117,11 @@ const Cart = () => {
                 <b>₹ {cartTotal.toLocaleString()}</b>
               </h3>
 
-              <button type="button" className="checkout_btn" disabled={cartQuantity === 0}>
+              <button
+                type="button"
+                className="checkout_btn"
+                disabled={cartQuantity === 0}
+              >
                 Checkout
               </button>
             </div>
